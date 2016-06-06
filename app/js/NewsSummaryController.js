02f718e795a59d0d2169dc232c83e282ad1751a3
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-06 10:41:11 +0100

    tidied up a bit

--- app/js/NewsSummaryController.js
+++ app/js/NewsSummaryController.js
@@ -1,16 +1,16 @@
 angular.module("newsSummaryApp")
   .controller("NewsSummaryController", ['ArticleFactory', 'ArticleService', '$routeParams', function(ArticleFactory, ArticleService, $routeParams) {
     var self = this;
+
     var newsSummaryApiUrl = 'http://news-summary-api.herokuapp.com/'
     var guardianApiRequestUrl = 'http://content.guardianapis.com/search?show-fields=thumbnail,headline,trailText&type=article&page-size=12&section=politics'
-
-    self.articles = [];
 
     self.setArticles = function() {
       self.articles = ArticleService.getArticlesData(newsSummaryApiUrl, 'guardian', guardianApiRequestUrl);
     }
 
-    // self.currentArticle = self.articles[$routeParams.id];
+    self.setArticles();
 
+    // self.currentArticle = self.articles[$routeParams.id];
 
   }]);
--- test/unit/ArticleService.spec.js
+++ test/unit/ArticleService.spec.js
@@ -6,17 +6,11 @@ describe('ArticleService', function() {
   var guardianResponseData = {
     data: {
       response: {
-        status: "ok",
         results: [{
-          webTitle: "Poll gives Brexit campaign three percentage points lead",
-          webPublicationDate: "2016-06-04T21:23:51Z",
           fields: {
             headline: "Poll gives Brexit campaign three percentage points lead",
-            trailText: "Opinium survey also shows extent of age and class divisions between leave and remain camps",
             thumbnail: "https://media.guim.co.uk/ffd24d7a177d54266c42f075427d37b35ee958f6/0_141_2800_1680/500.jpg"
-          },
-          webUrl: "https://www.theguardian.com/politics/2016/jun/04/poll-eu-brexit-lead-opinium",
-          sectionName: "Politics"
+          }
         }]
       }
     }
@@ -28,12 +22,12 @@ describe('ArticleService', function() {
     httpBackend = $httpBackend;
   }));
 
-  // it('makes a new array of articles using data from the guardian api', function(){
+  it('makes a new array of articles using data from the guardian api', function(){
 
-  //   httpBackend.expectGET('http://www.test.com').respond(guardianResponseData);
+    httpBackend.expectGET('http://www.test.com/service?apiRequestUrl=params').respond(guardianResponseData);
 
-  //   expect(ArticleService.getArticlesData('http://www.test.com')).toEqual([new ArticleFactory('Poll gives Brexit campaign three percentage points lead')]);
+    expect(ArticleService.getArticlesData('http://www.test.com', 'service', 'params')).toEqual([new ArticleFactory('Poll gives Brexit campaign three percentage points lead', 'https://media.guim.co.uk/ffd24d7a177d54266c42f075427d37b35ee958f6/0_141_2800_1680/500.jpg')]);
 
-  //   httpBackend.flush();
-  // });
+    httpBackend.flush();
+  });
 });
