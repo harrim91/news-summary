8ae3366ba82c4abd9813fabb648b03f1ef12e701
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-06 14:20:24 +0100

    got the opening a paticular article working

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,9 @@ var newsSummaryApp = angular.module("newsSummaryApp", ['ngRoute']);
 newsSummaryApp.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      controller:'NewsSummaryController',
       templateUrl: 'views/home.html'
     })
     .when('/summary/:id', {
-      controller: 'NewsSummaryController',
       templateUrl: 'views/summary.html'
     })
     .otherwise({
--- app/js/controllers/NewsSummaryController.js
+++ app/js/controllers/NewsSummaryController.js
@@ -1,15 +1,16 @@
 angular.module("newsSummaryApp")
   .controller("NewsSummaryController", ['ArticleFactory', 'ArticleService', '$routeParams', function(ArticleFactory, ArticleService, $routeParams) {
-    var self = this;
 
+    var self = this;
     var newsSummaryApiUrl = 'http://news-summary-api.herokuapp.com/'
     var guardianApiRequestUrl = 'http://content.guardianapis.com/search?show-fields=thumbnail,headline,trailText&type=article&page-size=12&section=politics'
 
-    var setArticles = function() {
-      self.articles = ArticleService.getArticlesData(
-        newsSummaryApiUrl, 'guardian', guardianApiRequestUrl);
-    }
+    self.setCurrentArticle = setCurrentArticle;
+    self.articles = ArticleService.getArticlesData(newsSummaryApiUrl, 'guardian', guardianApiRequestUrl);
 
-    setArticles();
+
+    function setCurrentArticle(index) {
+      self.currentArticle = self.articles[index];
+    }
 
   }]);
